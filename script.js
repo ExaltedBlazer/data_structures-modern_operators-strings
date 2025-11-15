25116e4648416d65d40ff69fd11b73b082955688
Kaden Horrell-St.Cloud <death5819@gmail.com>
2025-11-14 20:00:04 -0500

    Forth commit for strings in cloned repository

--- script.js
+++ script.js
@@ -102,6 +102,77 @@ const airline = 'TAP Air Portugal'
 console.log(airline.toLowerCase());
 console.log(airline.toUpperCase());
 
+// Fix capitalization in name
+const passenger = 'jOnAs'; // Jonas
+// const passengerLower = passenger.toLowerCase();
+// const passengerCorrect = passengerLower[0].toUpperCase() + passengerLower.slice(1);
+
+function capitalizedWord(capital) {
+  const toCapital = capital.toLowerCase()
+  return toCapital[0].toUpperCase() + toCapital.slice(1);
+}
+
+const toCapitalResult = capitalizedWord(passenger)
+
+console.log(toCapitalResult);
+
+// Comparing  emails
+
+const email = 'hello@Jonas.io';
+const loginEmail = ' Hello@Jonas.io \n'
+
+// const lowerEmail = loginEmail.toLowerCase();
+// const trimmedEmail = loginEmail.trim();
+// console.log(trimmedEmail);
+
+
+
+const normalizedEmail = loginEmail.toLowerCase().trim();
+
+console.log(normalizedEmail);
+console.log(email.toLowerCase() === normalizedEmail);
+
+//  Replacing
+
+const priceGB = '288,97£'
+const priceUS = priceGB.replace('£', '$').replace(',', '.');
+
+console.log(priceUS)
+
+const announcement = 'All passengers come to boarding door 23. Boarding door 23!';
+
+console.log(announcement.replace('door', 'gate'));
+//console.log(announcement.replaceAll('door', 'gate'));
+
+console.log(announcement.replace(/door/g, 'gate'));
+
+// Booleans
+const plane = 'Airbus  A320neo'
+console.log(plane.includes('A320'));
+console.log(plane.includes('Boeing'));
+console.log(plane.startsWith('Airb'));
+
+if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+  console.log('Part of the New Airbus family');
+
+}
+
+// Practice exercise
+
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+  if (baggage.includes('gun') || baggage.includes('knife')) {
+    console.log('You are NOT permitted aboard');
+
+  } else {
+    console.log('Welcome aboard!');
+
+  }
+}
+checkBaggage('I  have a laptop, some Food and a Pocket Knife');
+checkBaggage('Socks and camera');
+checkBaggage('Got some snacks and a gun for protection');
+
 /*
 //// Working with strings 1
 const plane = 'A320';
