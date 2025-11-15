48173ade04add1fdc503566a98e26b9d88c4f1d4
Kaden Horrell-St.Cloud <death5819@gmail.com>
2025-11-14 19:05:48 -0500

    Third commit for strings in cloned repository

--- script.js
+++ script.js
@@ -99,7 +99,48 @@ const restaurant = {
 
 const airline = 'TAP Air Portugal'
 
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+
 /*
+//// Working with strings 1
+const plane = 'A320';
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log('B737'[0]);
+
+console.log(airline.length);
+console.log('B737'.length);
+
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf('portugal'));
+
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+  // B and E are middle seats
+  const s = seat.slice(-1)
+  if (s === 'B' || s === 'E')
+    console.log('You got the middle seat');
+  else console.log('You got lucky');
+
+
+}
+
+checkMiddleSeat('11B')
+checkMiddleSeat('23C')
+checkMiddleSeat('3E')
+
 //// Challenge 3
 
 const gameEvents = new Map([
